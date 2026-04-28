88e6db6a203f5734c653ce6ace00fdc6879f8711
Ruth <Ruth3579@gmail.com>
2026-04-29 01:32:30 +0200

    Update filter for card service

--- Collect-Market/src/app/services/cards/cards-service.ts
+++ Collect-Market/src/app/services/cards/cards-service.ts
@@ -1,6 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { ModelCard } from '../../models/card';
+import { ModelFilteredCards } from '../../models/filter';
 
 @Injectable({
   providedIn: 'root',
@@ -11,20 +13,43 @@ export class CardsService {
 
   constructor(private http: HttpClient) {}
 
-   getCards(): Observable<any[]> {
-    return this.http.get<any[]>(`${this.apiUrl}/cards`);
+   getCards(): Observable<ModelCard[]> {
+    return this.http.get<ModelCard[]>(`${this.apiUrl}/cards`);
   }
 
-  getCardById(id: number): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/cards/${id}`);
+  getCardById(id: number): Observable<ModelCard> {
+    return this.http.get<ModelCard>(`${this.apiUrl}/cards/${id}`);
   }
 
-  getCardsByRarity(rarity: string): Observable<any[]> {
-    return this.http.get<any[]>(`${this.apiUrl}/cards/rarity/${rarity}`);
+  getCardsByRarity(rarity: string): Observable<ModelCard[]> {
+    return this.http.get<ModelCard[]>(`${this.apiUrl}/cards/rarity/${rarity}`);
   }
 
-  searchCards(term: string): Observable<any[]> {
-    return this.http.get<any[]>(`${this.apiUrl}/cards/search/${term}`);
+  searchCards(term: string): Observable<ModelCard[]> {
+    return this.http.get<ModelCard[]>(`${this.apiUrl}/cards/search/${term}`);
+  }
+
+    getFilteredCards(filters: ModelFilteredCards): Observable<ModelCard[]> {
+    let params = new HttpParams();
+
+    filters.rarities?.forEach(r => params = params.append('rarity', r));
+    filters.types?.forEach(t    => params = params.append('type', t));
+    filters.setNames?.forEach(s => params = params.append('setName', s));
+
+    if (filters.minPrice != null) params = params.set('minPrice', filters.minPrice);
+    if (filters.maxPrice != null) params = params.set('maxPrice', filters.maxPrice);
+
+    return this.http.get<ModelCard[]>(`${this.apiUrl}/cards/filter`, { params });
+  }
+
+  getTypes(): Observable<string[]> {
+  return this.http.get<string[]>(`${this.apiUrl}/cards/types`);
+  }
+  getRarities(): Observable<string[]> {
+    return this.http.get<string[]>(`${this.apiUrl}/cards/rarities`);
+  }
+  getExpansions(): Observable<string[]> {
+    return this.http.get<string[]>(`${this.apiUrl}/cards/expansions`);
   }
   
 }
