454575aceb56b83ee4ee04c466009a5c496b4e3d
Ruth <Ruth3579@gmail.com>
2026-04-29 01:37:00 +0200

    Update routes

--- Collect-Market/src/app/app.routes.ts
+++ Collect-Market/src/app/app.routes.ts
@@ -3,13 +3,15 @@ import { HomePage } from './pages/home-page/home-page';
 import { LoginPage } from './pages/login-page/login-page';
 import { RegisterPage } from './pages/register-page/register-page';
 import { UserPage } from './pages/user-page/user-page';
+import { ShopPage } from './pages/shop-page/shop-page';
 
 export const routes: Routes = [
     {path:"", component: HomePage},
     {path:"home", component: HomePage},
     {path:"auth/login", component: LoginPage},
     {path:"auth/register", component: RegisterPage},
-    {path:"user", component: UserPage}
+    {path:"user", component: UserPage},
+    {path:"shop", component: ShopPage}
     
 
 ];
