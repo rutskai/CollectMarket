466079cb81e837701c73c9d3488286af46419ee0
Ruth <rcolladogarcia@iessonferrer.net>
2026-05-03 18:00:14 +0200

    Update cards for home

--- Collect-Market/src/app/pages/home-page/home-page.ts
+++ Collect-Market/src/app/pages/home-page/home-page.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { CardsService } from '../../services/cards/cards-service';
+import { RouterLink } from '@angular/router';
 declare var $: any;
 
 @Component({
   selector: 'app-home-page',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
@@ -14,33 +15,33 @@ export class HomePage implements OnInit {
   cards: any[] = [];
   popularCards: any[] = [];
 
-  constructor(private cardService: CardsService) {}
+  constructor(private cardService: CardsService, private cdr: ChangeDetectorRef) {}
 
   ngOnInit(): void {
     $('#home').vide({ mp4: 'video/poke-center.gif' });
-
     this.loadCards();
   }
+ 
 
   loadCards(): void {
     this.cardService.getCards().subscribe({
       next: (data) => {
-        const mapped = data.map(card => ({
-          ...card,
-          imageUrl: `${card.imageUrl}/low.jpg`
-        }));
-
-        this.cards = mapped;
-
-       
-        const shuffled = [...mapped].sort(() => 0.6 - Math.random());
-        this.popularCards = shuffled.slice(0, 6);
-
-        console.log('Cartas populares:', this.popularCards);
+        const shuffled = [...data].sort(() => 0.6 - Math.random());
+        this.cards = shuffled.slice(0, 3);
+        this.popularCards = shuffled.slice(0, 5);
+        this.cdr.detectChanges();
       },
-      error: (err) => {
-        console.error('Error cargando cartas:', err);
-      }
+      error: (err) => console.error('Error cargando cartas:', err)
     });
   }
+
+   getImageUrl(card: any): string {
+  if (!card.imageUrl) return 'assets/card-placeholder.png';
+  if (card.imageUrl.endsWith('.png') || 
+      card.imageUrl.endsWith('.jpg') || 
+      card.imageUrl.endsWith('.webp')) {
+    return card.imageUrl;
+  }
+  return `${card.imageUrl}/low.webp`;
+}
 }
