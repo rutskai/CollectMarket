a203ca1a00f38c3c59fbb9a8536a26d6e44aa0f3
Ruth <Ruth3579@gmail.com>
2026-05-18 20:57:23 +0200

    Fix bug in add cart

--- Collect-Market/src/app/components/card/card.ts
+++ Collect-Market/src/app/components/card/card.ts
@@ -1,34 +1,42 @@
-import { Component, EventEmitter, Output, Input} from '@angular/core';
-import { ModelCard } from '../../models/card';
-import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
+  import { Component, EventEmitter, Output, Input} from '@angular/core';
+  import { ModelCard } from '../../models/card';
+  import { CommonModule } from '@angular/common';
+  import { HttpClient } from '@angular/common/http';
 
 
-@Component({
-  selector: 'app-card',
-  imports: [CommonModule],
-  templateUrl: './card.html',
-  styleUrl: './card.css',
-})
-export class Card {
+  @Component({
+    selector: 'app-card',
+    imports: [CommonModule],
+    templateUrl: './card.html',
+    styleUrl: './card.css',
+  })
+  export class Card {
 
-  constructor(private http: HttpClient){}
+    constructor(private http: HttpClient){}
 
-  @Input() card!: ModelCard;
-  @Input() isFavorite: boolean = false;
-  @Input() isInCart: boolean = false;
-  @Output() addToCart = new EventEmitter<ModelCard>();
-  @Output() toggleFavorite = new EventEmitter<ModelCard>();
+    @Input() card!: ModelCard;
+    @Input() isFavorite: boolean = false;
+    @Input() isInCart: boolean = false;
+    @Output() addToCart = new EventEmitter<ModelCard>();
+    @Output() toggleFavorite = new EventEmitter<ModelCard>();
 
-  get imageUrl(): string {
-    if (!this.card.imageUrl) return 'assets/card-placeholder.png';
-    if (this.card.imageUrl.endsWith('.png') || 
-        this.card.imageUrl.endsWith('.jpg') || 
-        this.card.imageUrl.endsWith('.webp')) {
-      return this.card.imageUrl;
+    get imageUrl(): string {
+      if (!this.card.imageUrl) return 'assets/card-placeholder.png';
+      if (this.card.imageUrl.endsWith('.png') || 
+          this.card.imageUrl.endsWith('.jpg') || 
+          this.card.imageUrl.endsWith('.webp')) {
+        return this.card.imageUrl;
+      }
+      return `${this.card.imageUrl}/high.webp`; 
     }
-    return `${this.card.imageUrl}/high.webp`; 
+onAddToCart(): void {
+    console.log('Card component emitiendo addToCart para:', this.card.id);
+    this.addToCart.emit(this.card);
   }
 
+  onToggleFavorite(): void {
+    console.log('Card component emitiendo toggleFavorite para:', this.card.id);
+    this.toggleFavorite.emit(this.card);
+  }
 
-}
+  }
