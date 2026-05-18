dd3201d6154b1db94afcd2b2057a4750b80020b3
Ruth <rcolladogarcia@iessonferrer.net>
2026-05-18 02:06:43 +0200

    Add personalization-page to routes

--- Collect-Market/src/app/app.routes.ts
+++ Collect-Market/src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { ShopPage } from './pages/shop-page/shop-page';
 import { FavoritePage } from './pages/favorite-page/favorite-page';
 import { ShoppingCartPage } from './pages/shopping-cart-page/shopping-cart-page';
 import { SupportPage } from './pages/support-page/support-page';
+import { UserPersonalizationPage } from './pages/user-personalization-page/user-personalization-page';
 
 export const routes: Routes = [
     {path:"", component: HomePage},
@@ -17,7 +18,8 @@ export const routes: Routes = [
     {path:"shop", component: ShopPage},
     {path:"favorite", component: FavoritePage},
     {path: "shopping-cart", component:ShoppingCartPage},
-    {path:"support-page", component:SupportPage}
+    {path:"support-page", component:SupportPage},
+    {path: 'personalization-page', component: UserPersonalizationPage },
     
 
 ];
