99ace2b4b716f3eb9253aa38739f9af059d38838
Ruth <Ruth3579@gmail.com>
2026-05-19 16:30:09 +0200

    Update reactive forms

--- Collect-Market/src/app/components/login-form/login-form.ts
+++ Collect-Market/src/app/components/login-form/login-form.ts
@@ -1,42 +1,55 @@
-  import { Component, OnInit } from '@angular/core';
-  import { FormsModule } from '@angular/forms';
+  import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+  import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
   import { AuthService } from '../../services/auth/auth-service';
-  import { Router } from '@angular/router';
+  import { Router, RouterLink } from '@angular/router';
   declare var $: any;
 
   @Component({
     selector: 'app-login-form',
-    imports: [FormsModule],
+    imports: [ReactiveFormsModule, RouterLink],
     templateUrl: './login-form.html',
     styleUrl: './login-form.css',
   })
   export class LoginForm implements OnInit {
 
-    credentials = {
-      email: '',
-      password: ''
-    };
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)])
+  });
+  
+  errorMessage = '';
 
-    errorMessage: string = '';
-
-    constructor(private authService: AuthService, private router: Router) { }
+  constructor(private authService: AuthService, private router: Router, private cdr:ChangeDetectorRef) {}
 
     ngOnInit(): void {
       $('#login').vide({ mp4: 'video/pickachu_runtime' });
     }
 
-    onSubmit(): void {
-      this.authService.login(this.credentials.email, this.credentials.password).subscribe({
-        next: (response) => {
-          this.authService.setToken(response.token);
-          this.authService.setUser(response.user);
-          console.log("Usuario logueado correctamente.");
-          this.router.navigate(['/home']);
-        },
+  onSubmit(): void {
+    if (this.loginForm.invalid) return;
+
+    const { email, password } = this.loginForm.value;
+
+    this.authService.login(email!, password!).subscribe({
+      next: (response) => {
+        this.authService.setToken(response.token);
+        this.authService.setUser(response.user);
+        console.log("Usuario logueado correctamente.");
+        this.loginForm.reset();
+        this.errorMessage = '';
+        this.router.navigate(['/home']);
+      },
         error: (err) => {
-          this.errorMessage = err.error?.message || 'Error al iniciar sesión';
-        }
-      });
-    }
+  if (err.status === 404) {
+    this.errorMessage = err.error?.message || 'Email no encontrado';
+  } else if (err.status === 401) {
+    this.errorMessage = err.error?.message || 'Contraseña incorrecta';
+  } else {
+    this.errorMessage = err.error?.message || 'Error al iniciar sesión';
+  }
+  this.cdr.detectChanges();
+}
+    });
+  }
 
   }
